b4a8d774984a5888d21831579bb938c956b0174f
Michał <michal.syc1990@gmail.com>
2024-05-04 22:53:07 +0200

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,28 @@
 import Logo from "../images/logo.svg";
 import { pageLinks, socialLinks } from "../data";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const handleMenuOpen = () => {
+    setOpenMenu(!openMenu);
+  };
+
+  const handleMenuClose = () => {
+    setOpenMenu(false);
+  };
   return (
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
           <img src={Logo} className="nav-logo" alt="backroads" />
-          <button type="button" className="nav-toggle" id="nav-toggle">
+          <button
+            type="button"
+            className="nav-toggle"
+            onClick={handleMenuOpen}
+            id="nav-toggle"
+          >
             <i className="fas fa-bars"></i>
           </button>
         </div>
@@ -23,6 +38,23 @@ const Navbar = () => {
             );
           })}
         </ul>
+        {openMenu && (
+          <ul className="nav-links-mobile" id="nav-links">
+            {pageLinks.map((link) => {
+              return (
+                <li key={link.id}>
+                  <a
+                    href={link.href}
+                    className="nav-link"
+                    onClick={handleMenuClose}
+                  >
+                    {link.text}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        )}
 
         <ul className="nav-icons">
           {socialLinks.map((link) => {
